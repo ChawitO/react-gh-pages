bd543ce5bf56adfae66b7615a95f4fd0cf17d2df
Chawit.O <chawit.ocr@gmail.com>
2019-11-30 12:30:27 +0000

    test setting up react app with GithubPages

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Nav extends React.Component {
+  state = {
+    data: null
+  }
+
+  render() {
+    return (
+      <nav>This be the nav bar</nav>
+    )
+  }
+}
