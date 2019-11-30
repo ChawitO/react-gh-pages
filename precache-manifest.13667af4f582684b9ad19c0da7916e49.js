5c4b5cc328710dbebb55318e22c83a99fa6e316a
Chawit.O <chawit.ocr@gmail.com>
2019-11-30 11:56:09 +0000

    Updates

---
+++ precache-manifest.13667af4f582684b9ad19c0da7916e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf6b2c100c1ad5a6e270584f26bc9df",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a54d612688e39c63e62d",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3d71cd511a6f7ae9e297",
+    "url": "/react-gh-pages/static/js/2.0eb277ec.chunk.js"
+  },
+  {
+    "revision": "a54d612688e39c63e62d",
+    "url": "/react-gh-pages/static/js/main.9ffffbf8.chunk.js"
+  },
+  {
+    "revision": "c6a306b69a318852ef4c",
+    "url": "/react-gh-pages/static/js/runtime-main.f5d2195b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
